d56ac3a1b8d44d26d228e827b88f5d8ef626bc27
xxchenxx <hfercxx@hotmail.com>
2022-07-24 10:13:38 -0500

    update

--- configs/convnext/cascade_mask_rcnn_slak_small_patch4_window7_mstrain_480-800_giou_4conv1f_adamw_3x_voc_in1k.py
+++ configs/convnext/cascade_mask_rcnn_slak_small_patch4_window7_mstrain_480-800_giou_4conv1f_adamw_3x_voc_in1k.py
@@ -128,7 +128,7 @@ train_pipeline = [
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
 ]
-data = dict(train=dict(pipeline=train_pipeline), samples_per_gpu=4, workers_per_gpu=2)
+data = dict(train=dict(pipeline=train_pipeline), samples_per_gpu=2, workers_per_gpu=4)
 
 optimizer = dict(constructor='LearningRateDecayOptimizerConstructor', _delete_=True, type='AdamW', 
                  lr=0.0002, betas=(0.9, 0.999), weight_decay=0.05,
